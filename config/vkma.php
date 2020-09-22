0cdcf11178a09012ae3506466ae5f9d14278c5e9
Evgeniy Zavalishin <ezavalishin@gmail.com>
2020-09-23 00:44:13 +0400

    fix default values

--- config/vkma.php
+++ config/vkma.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'app_id' => env('VKMA_APP_ID'),
-    'app_secret' => env('VKMA_APP_SECRET'),
-    'service_key' => env('VKMA_SERVICE_KEY'),
+    'app_id' => env('VKMA_APP_ID', 0),
+    'app_secret' => env('VKMA_APP_SECRET', ''),
+    'service_key' => env('VKMA_SERVICE_KEY', ''),
 
     'options' => [
         'model' => \App\User::class,
