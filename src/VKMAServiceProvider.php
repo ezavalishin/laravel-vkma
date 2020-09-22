658f51a171a2a950e4581618e11d1ab176d367da
Evgeniy Zavalishin <ezavalishin@gmail.com>
2020-09-23 01:17:54 +0400

    fix sp

--- src/VKMAServiceProvider.php
+++ src/VKMAServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace ezavalishin\VKMA;
 
-use ezavalishin\Guards\VKMAGuard;
+use ezavalishin\VKMA\Guards\VKMAGuard;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
